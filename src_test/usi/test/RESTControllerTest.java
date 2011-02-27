9873f4ab384a984865bd0076c848688cc6ce6917
tabasco <guillaume.giamarchi@gmail.com>
2011-02-27 15:44:53 +0100

    Adding controler for testing queries

---
+++ src_test/usi/test/RESTControllerTest.java
@@ -0,0 +1,45 @@
+package usi.test;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import usi.poc.business.itf.IGame;
+import usi.poc.business.itf.User;
+
+@Controller
+public class RESTControllerTest {
+
+	@Resource
+	private IGame game;
+
+	@Resource
+	private Map<String, User> usersCache;
+	
+
+	@RequestMapping(value="/test/user/{email}", method=RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public User user(@PathVariable String email) throws Exception {
+		System.out.println("RESTControllerTest.user()");
+		System.out.println(usersCache);
+		return usersCache.get(email);
+	}
+
+	@RequestMapping(value="/test/usercount", method=RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public int userCount() throws Exception {
+		System.out.println("RESTControllerTest.userCount()");
+		return usersCache.size();
+	}
+	
+}
