f5400ea544707038a2ec1a01f9af2fcdb069f7da
bouledeturquie <marc.lucchini@gmail.com>
2011-03-01 19:57:44 +0100

    Ooops ! (j'ai oublié SessionUtils.java)

---
+++ src/usi/SessionUtils.java
@@ -0,0 +1,25 @@
+package usi;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import usi.poc.business.itf.IGame;
+import usi.poc.business.itf.User;
+
+public class SessionUtils {
+	public static final String SESSION_KEY = "session_key";
+	
+	public static User getLoggedUser(HttpServletRequest request, IGame game) {
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals(SESSION_KEY)) {
+				User user = game.getUser(cookie.getValue());
+				if (user != null && user.isLogged())
+					return user;
+				else
+					return null;
+			}
+		}
+		return null;
+	}
+}
