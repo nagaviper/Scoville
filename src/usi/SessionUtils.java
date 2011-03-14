d5caa767406af2fd516cda33e4f83f8bd5f07726
tabasco <guillaume.giamarchi@gmail.com>
2011-03-14 22:20:43 +0100

    NullPointerException bug correction on authentication cookie research when there is no cookie.

--- src/usi/SessionUtils.java
+++ src/usi/SessionUtils.java
@@ -10,7 +10,10 @@ public class SessionUtils {
 	public static final String SESSION_KEY = "session_key";
 	
 	public static User getLoggedUser(HttpServletRequest request, IGame game) {
-		Cookie[] cookies = request.getCookies();
+		Cookie[] cookies = request.getCookies(); // return null if there is no cookie
+		if (cookies == null) {
+			return null;
+		}
 		for (Cookie cookie : cookies) {
 			if (cookie.getName().equals(SESSION_KEY)) {
 				User user = game.getUser(cookie.getValue());
